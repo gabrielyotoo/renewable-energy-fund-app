12483f21219f1adce930a6d40d26a3cd4f5608f0
Gabriel Freitas Yamamoto <gabrielyamamoto82@gmail.com>
2023-07-15 19:32:41 -0300

    fix(header): add back navigation and style

--- src/components/header/header.style.ts
+++ src/components/header/header.style.ts
@@ -1,6 +1,15 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+  icon: {
+    position: 'absolute',
+    paddingVertical: 30,
+    paddingHorizontal: 25,
+  },
+  iconContainer: {
+    justifyContent: 'center',
+    paddingVertical: 30,
+  },
   container: {
     paddingVertical: 30,
   },
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,7 @@
 import { Ionicons } from '@expo/vector-icons';
 import { useTheme } from '@react-navigation/native';
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
-import { View } from 'react-native';
+import { TouchableNativeFeedback, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import styles from './header.style';
@@ -11,11 +11,21 @@ const Header = ({ navigation }: NativeStackHeaderProps) => {
 
   return (
     <SafeAreaView edges={['top']}>
-      <View style={styles.container}>
-        {navigation.canGoBack() ? (
-          <Ionicons name="ios-arrow-back" size={24} color="black" />
-        ) : null}
-      </View>
+      {navigation.canGoBack() ? (
+        <View style={styles.iconContainer}>
+          <TouchableNativeFeedback onPress={navigation.goBack}>
+            <Ionicons
+              style={styles.icon}
+              name="ios-arrow-back"
+              size={28}
+              color={colors.text}
+            />
+          </TouchableNativeFeedback>
+        </View>
+      ) : (
+        <View style={styles.container} />
+      )}
+
       <View
         style={{ ...styles.separator, backgroundColor: colors.textInput }}
       />
